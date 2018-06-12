565d1db0133669882dd8c5c2cf890602fba0bb74
admin <admin@TA-822-05>
2018-06-12 15:43:51 +0200

    .

---
+++ Delegate 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_1
+{
+    class Program
+    {
+        public delegate int BeregnDelegate(int a, int b);
+
+        static void Main(string[] args)
+        {
+            BeregnDelegate f = Plus;
+            int res1 = Beregner(100, 1, f);
+            int res = Beregner(3, 1, Minus);
+            Console.WriteLine(res1);
+        }
+
+
+        public static int Beregner(int x, int z, BeregnDelegate funktion)
+        {
+            return funktion(x, z);
+        }
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Divider(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Gange(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
---
+++ Delegates 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maskine m = new Maskine();
+            m.Log = Console.WriteLine;
+            //m.Log += AppendLog;
+            //m.Log += delegate (string txt) { System.IO.File.AppendAllText(@"c:/temp/log.txt" txt + "\r\n"); };
+            m.Start();
+            m.Stop();
+          
+
+
+        }
+
+    }
+
+    public delegate void LogDelegate(string txt);
+    public class Maskine
+    {
+        //public LogDelegate Log { get; set; }
+        public Action<string> Log { get; set; }
+        public void Start()
+        {
+            Log(DateTime.Now.ToLongTimeString() + " Starter");
+        }
+        public void Stop()
+        {
+            Log(DateTime.Now.ToLongTimeString() + " Stopper");
+        }
+    }
+
+
+}
